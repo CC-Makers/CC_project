0a48a3af8832d8baa4444d2f17109c34e968fa25
losecow <100589396+Losecow@users.noreply.github.com>
2025-06-23 14:00:47 +0900

    패키지 이름 변경 후 재업로드

---
+++ ccgo-backend/ccgo-backend/src/test/java/com/ccgo/backend/CcgoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccgo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcgoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
