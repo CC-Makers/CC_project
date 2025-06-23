6e2233b053c7f40b958bebd85ce3912657fe57ad
losecow <100589396+Losecow@users.noreply.github.com>
2025-06-23 13:14:40 +0900

    Add SpringBoot Project files

---
+++ ccgo/ccgo/src/main/java/ccgo/com/ccgo/CcgoApplication.java
@@ -0,0 +1,13 @@
+package ccgo.com.ccgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcgoApplication.class, args);
+	}
+
+}
---
+++ ccgo/ccgo/src/test/java/ccgo/com/ccgo/CcgoApplicationTests.java
@@ -0,0 +1,13 @@
+package ccgo.com.ccgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
